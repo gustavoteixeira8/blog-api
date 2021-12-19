01cefe3737a2f94bd68160dfbe96b94156ecec06
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-19 19:14:23 -0300

    Remove o método searchWithRelations e adiciona os métodos findAllPublicByUserWithRelations, findAllPublicByCategoryWithRelations e findAllPublicWithRelations

--- src/modules/articles/repositories/ArticleRepositoryProtocol.ts
+++ src/modules/articles/repositories/ArticleRepositoryProtocol.ts
@@ -6,30 +6,25 @@ import { RepositoryOptions } from '@shared/core/repositories/RepositoryOptions';
 import { ArticleWithRelationsDTO } from '../dtos/ArticleWithRelationsDTO';
 import { Article } from '../entities/Article';
 
-export interface SearchArticleOptions {
-  title?: string;
-  categorySlug?: string;
-  username?: string;
-  withDeleted?: boolean;
-  isPublic?: boolean;
-}
-
 export interface ArticleRepositoryProtocol {
   save(article: Article): Promise<void>;
   delete(articleId: string): Promise<void>;
   findById(articleId: string, options?: RepositoryOptions): Promise<Article | undefined>;
+  findAllDeleted(): Promise<Article[]>;
   existsWithSlug(slug: string, options?: RepositoryOptions): Promise<boolean>;
-  findForCreatorByIdWithRelations(
-    articleId: string,
-    userId: string,
-  ): Promise<ArticleWithRelationsDTO | undefined>;
   findPublicByIdWithRelations(
     articleId: string,
     options?: RepositoryOptions,
   ): Promise<ArticleWithRelationsDTO | undefined>;
-  searchWithRelations(
-    searchOptions: SearchArticleOptions,
+  findAllPublicWithRelations(
     pagination: PaginationOptionsProtocol,
   ): Promise<PaginationResponseProtocol<ArticleWithRelationsDTO>>;
-  findAllDeleted(): Promise<Article[]>;
+  findAllPublicByCategoryWithRelations(
+    categoryId: string,
+    pagination: PaginationOptionsProtocol,
+  ): Promise<PaginationResponseProtocol<ArticleWithRelationsDTO>>;
+  findAllPublicByUserWithRelations(
+    userId: string,
+    pagination: PaginationOptionsProtocol,
+  ): Promise<PaginationResponseProtocol<ArticleWithRelationsDTO>>;
 }
--- src/modules/articles/repositories/implementations/ArticleRepositoryOrm.ts
+++ src/modules/articles/repositories/implementations/ArticleRepositoryOrm.ts
@@ -7,7 +7,7 @@ import {
 import { RepositoryOptions } from '@shared/core/repositories/RepositoryOptions';
 import { getRepository, IsNull, Not } from 'typeorm';
 import { ArticleEntity } from '@shared/infra/database/entities/ArticleEntity';
-import { ArticleRepositoryProtocol, SearchArticleOptions } from '../ArticleRepositoryProtocol';
+import { ArticleRepositoryProtocol } from '../ArticleRepositoryProtocol';
 import { ArticleWithRelationsDTO } from '@modules/articles/dtos/ArticleWithRelationsDTO';
 import { UserMapper } from '@modules/users/mappers/UserMapper';
 import { CategoryMapper } from '@modules/categories/mappers/CategoryMapper';
@@ -77,41 +77,20 @@ export class ArticleRepositoryOrm implements ArticleRepositoryProtocol {
     };
   }
 
-  public async findForCreatorByIdWithRelations(
-    articleId: string,
-    userId: string,
-  ): Promise<ArticleWithRelationsDTO | undefined> {
-    const article = await this._table.findOne({
-      join: {
-        alias: 'a',
-        leftJoinAndSelect: { user: 'a.user', categories: 'a.categories' },
-      },
-      where: { id: articleId, userId },
-      withDeleted: true,
-    });
-
-    if (!article) return;
-
-    return {
-      article: ArticleMapper.toDomain(article),
-      user: UserMapper.toDomain(article.user),
-      categories: article.categories.map(CategoryMapper.toDomain),
-    };
-  }
-
-  public async searchWithRelations(
-    searchOptions: SearchArticleOptions,
+  public async findAllPublicByCategoryWithRelations(
+    categoryId: string,
     pagination: PaginationOptionsProtocol,
   ): Promise<PaginationResponseProtocol<ArticleWithRelationsDTO>> {
     const { order, perPage, page } = pagination;
-    const { isPublic, categorySlug, username, title, withDeleted } = searchOptions;
 
     const qb = this._table
       .createQueryBuilder('a')
-      .take(perPage)
-      .skip(page)
       .leftJoinAndSelect('a.user', 'user')
-      .leftJoinAndSelect('a.categories', 'category');
+      .leftJoinAndSelect('a.categories', 'category')
+      .where('category.id = :categoryId', { categoryId })
+      .andWhere('a.isPublic = true')
+      .take(perPage)
+      .skip(page);
 
     if (order) {
       for (const key in order) {
@@ -119,27 +98,64 @@ export class ArticleRepositoryOrm implements ArticleRepositoryProtocol {
       }
     }
 
-    if (withDeleted === true) {
-      qb.withDeleted();
-    }
-
-    if (title) {
-      qb.andWhere('a.title like :title', { title: `%${title}%` });
-    }
-
-    if (isPublic !== undefined) {
-      qb.andWhere('a.isPublic = :isPublic', { isPublic });
-    }
-
-    if (categorySlug) {
-      qb.andWhere('category.slug = :categorySlug', { categorySlug });
-    }
-
-    if (username) {
-      qb.andWhere('user.username = :username', { username });
-    }
-
     const [articles, count] = await qb.getManyAndCount();
+
+    return {
+      data: articles.map((article) => {
+        const user = UserMapper.toDomain(article.user);
+        const categories = article.categories.map(CategoryMapper.toDomain);
+
+        return { article: ArticleMapper.toDomain(article), user, categories };
+      }),
+      page: Math.ceil(page / perPage + 1),
+      perPage,
+      maxItems: count,
+      maxPage: Math.ceil(count / perPage),
+      order: order || null,
+    };
+  }
+
+  public async findAllPublicWithRelations(
+    pagination: PaginationOptionsProtocol,
+  ): Promise<PaginationResponseProtocol<ArticleWithRelationsDTO>> {
+    const { order, perPage, page } = pagination;
+
+    const [articles, count] = await this._table.findAndCount({
+      where: { isPublic: true },
+      join: { alias: 'a', leftJoinAndSelect: { user: 'a.user', categories: 'a.categories' } },
+      order,
+      skip: page,
+      take: perPage,
+    });
+
+    return {
+      data: articles.map((article) => {
+        const user = UserMapper.toDomain(article.user);
+        const categories = article.categories.map(CategoryMapper.toDomain);
+
+        return { article: ArticleMapper.toDomain(article), user, categories };
+      }),
+      page: Math.ceil(page / perPage + 1),
+      perPage,
+      maxItems: count,
+      maxPage: Math.ceil(count / perPage),
+      order: order || null,
+    };
+  }
+
+  public async findAllPublicByUserWithRelations(
+    userId: string,
+    pagination: PaginationOptionsProtocol,
+  ): Promise<PaginationResponseProtocol<ArticleWithRelationsDTO>> {
+    const { order, perPage, page } = pagination;
+
+    const [articles, count] = await this._table.findAndCount({
+      join: { alias: 'a', leftJoinAndSelect: { user: 'a.user', categories: 'a.categories' } },
+      where: { isPublic: true, user: { id: userId } },
+      order,
+      skip: page,
+      take: perPage,
+    });
 
     return {
       data: articles.map((article) => {
