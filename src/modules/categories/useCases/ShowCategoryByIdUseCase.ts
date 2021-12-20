56b5d4467b98f7e594506394c2e387c3eadce9e0
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-20 15:57:08 -0300

    Altera o nome da internface ShowCategoryRequest

--- src/modules/categories/useCases/ShowCategoryByIdUseCase.ts
+++ src/modules/categories/useCases/ShowCategoryByIdUseCase.ts
@@ -4,20 +4,20 @@ import { CategoryRepositoryProtocol } from '../repositories/CategoryRepositoryPr
 import { Category } from '../entities/Category';
 import { CategoryNotFoundError, MissingParamError } from '@shared/core/errors';
 
-export interface ShowCategoryByIdRequest {
+export interface ShowCategoryRequest {
   categoryId: string;
 }
 
 @injectable()
 export class ShowCategoryByIdUseCase
-  implements UseCaseProtocol<ShowCategoryByIdRequest, Promise<Category>>
+  implements UseCaseProtocol<ShowCategoryRequest, Promise<Category>>
 {
   constructor(
     @inject('CategoryRepository')
     private readonly _categoryRepository: CategoryRepositoryProtocol,
   ) {}
 
-  public async execute({ categoryId }: ShowCategoryByIdRequest): Promise<Category> {
+  public async execute({ categoryId }: ShowCategoryRequest): Promise<Category> {
     if (!categoryId) throw new MissingParamError('Category id');
 
     const category = await this._categoryRepository.findById(categoryId);
