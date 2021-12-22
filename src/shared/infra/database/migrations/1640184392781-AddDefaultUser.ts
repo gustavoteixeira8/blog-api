c23c395ed1abbe11d1b83b71b8ee8b325438e4cb
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-22 11:57:57 -0300

    Adiciona migration para criar um usuário padrão no banco de dados

---
+++ src/shared/infra/database/migrations/1640184392781-AddDefaultUser.ts
@@ -0,0 +1,27 @@
+import { BCryptProvider } from '@shared/providers/hashProvider/implementations/BCryptProvider';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { UserEntity } from '../entities/UserEntity';
+
+export class AddDefaultUser1640184392781 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager.save(
+      queryRunner.manager.create(UserEntity, {
+        id: uuid(),
+        fullName: process.env.DEFAULT_USER_FULLNAME,
+        email: process.env.DEFAULT_USER_EMAIL,
+        username: process.env.DEFAULT_USER_USERNAME,
+        password: await new BCryptProvider().generate(process.env.DEFAULT_USER_PASSWORD as string),
+        isAdmin: true,
+        isEmailVerified: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+      }),
+    );
+  }
+
+  public async down(): Promise<void> {
+    //
+  }
+}
