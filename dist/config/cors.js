4b2314aab7e8303017fc9a747d48395e6a445238
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-23 10:32:59 -0300

    Adiciona CORS config para dist

---
+++ dist/config/cors.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.corsConfig = void 0;
+
+var _httpErrors = require("../shared/infra/http/errors/httpErrors");
+
+const allowedDomains = ['https://api.gustavo.gq', 'http://localhost:3000'];
+const corsConfig = {
+  origin: (origin, cb) => {
+    if (origin && !allowedDomains.includes(origin)) {
+      cb(new _httpErrors.UnauthorizedError('Not allowed by CORS'));
+      return;
+    }
+
+    cb(null, origin);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+};
+exports.corsConfig = corsConfig;
