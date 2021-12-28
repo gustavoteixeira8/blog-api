48fb0364b702041c2b43cd9f645d4fc267fddec4
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-12-28 12:27:03 -0300

    Adiciona o middleware sanitizeBody em todos os endpoints

--- src/shared/infra/http/routes/index.routes.ts
+++ src/shared/infra/http/routes/index.routes.ts
@@ -4,13 +4,14 @@ import { authRoutes } from '@modules/users/infra/http/routes/auth.routes';
 import { categoryRoutes } from '@modules/categories/infra/http/routes/category.routes';
 import { articleRoutes } from '@modules/articles/infra/http/routes/article.routes';
 import { authRoutesLimiter, authRoutesSlowDown, defaultLimiter } from '../middlewares/rateLimiter';
+import { sanitizeBody } from '../middlewares/sanitizeBody';
 
 const routes = Router();
 
 routes
-  .use('/user', defaultLimiter, userRoutes)
-  .use('/auth', authRoutesSlowDown, authRoutesLimiter, authRoutes)
-  .use('/category', defaultLimiter, categoryRoutes)
-  .use('/article', defaultLimiter, articleRoutes);
+  .use('/user', sanitizeBody, defaultLimiter, userRoutes)
+  .use('/auth', sanitizeBody, authRoutesSlowDown, authRoutesLimiter, authRoutes)
+  .use('/category', sanitizeBody, defaultLimiter, categoryRoutes)
+  .use('/article', sanitizeBody, defaultLimiter, articleRoutes);
 
 export { routes };
