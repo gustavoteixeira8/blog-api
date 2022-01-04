dad98dbb5bdd3b9ce1e1c55b35c2e9ae9fc78a34
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-01-04 15:21:08 -0300

    Adiciona atributo de styles para tags HTML no SanitizeHTMLProvider

--- src/shared/providers/sanitizerProvider/implementations/SanitizeHtmlProvider.ts
+++ src/shared/providers/sanitizerProvider/implementations/SanitizeHtmlProvider.ts
@@ -1,10 +1,11 @@
 import { SanitizerProviderProtocol } from '../SanitizerProviderProtocol';
 import sanitizeHTML from 'sanitize-html';
+import { sanitizeConfig } from '@config/sanitize';
 
 export class SanitizeHtmlProvider implements SanitizerProviderProtocol {
   public sanitize(value: any): string | any {
     if (typeof value !== 'string') return value;
 
-    return sanitizeHTML(value);
+    return sanitizeHTML(value, sanitizeConfig);
   }
 }
