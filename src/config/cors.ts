7cc37d1d544cca19c37c75b78c20499d4f3611ce
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-01-11 17:25:53 -0300

    Remove URLs antigos do whiteList e adiciona a URL gustavo.gq

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,15 +1,11 @@
 import { UnauthorizedError } from '@shared/infra/http/errors/httpErrors';
 import { CorsOptions } from 'cors';
 
-const allowedDomains = [
-  'https://api.gustavo.gq',
-  'http://localhost:3000',
-  process.env.MY_LOCALHOST_IP,
-];
+const allowedDomains = ['https://gustavo.gq'];
 
 export const corsConfig = {
   origin: (origin, cb) => {
-    if (origin && !allowedDomains.includes(origin)) {
+    if (!allowedDomains.includes(origin || 'WRONG_ORIGIN')) {
       cb(new UnauthorizedError('Not allowed by CORS'));
       return;
     }
