1892abe033bccf796fca9d400742acc6bb9a8817
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-07-01 16:36:16 -0300

    Remove limiters

--- src/shared/infra/http/routes/index.routes.ts
+++ src/shared/infra/http/routes/index.routes.ts
@@ -3,17 +3,16 @@ import { userRoutes } from '@modules/users/infra/http/routes/user.routes';
 import { setupAuthRoutes } from '@modules/users/infra/http/routes/auth.routes';
 import { categoryRoutes } from '@modules/categories/infra/http/routes/category.routes';
 import { articleRoutes } from '@modules/articles/infra/http/routes/article.routes';
-import { authRoutesLimiter, authRoutesSlowDown, defaultLimiter } from '../middlewares/rateLimiter';
 import { sanitizeBody } from '../middlewares/sanitizeBody';
 
 export const setupRoutes = () => {
   const routes = Router();
 
   routes
-    .use('/user', sanitizeBody, defaultLimiter, userRoutes())
-    .use('/auth', sanitizeBody, authRoutesSlowDown, authRoutesLimiter, setupAuthRoutes())
-    .use('/category', sanitizeBody, defaultLimiter, categoryRoutes)
-    .use('/article', sanitizeBody, defaultLimiter, articleRoutes);
+    .use('/user', sanitizeBody, userRoutes())
+    .use('/auth', sanitizeBody, setupAuthRoutes())
+    .use('/category', sanitizeBody, categoryRoutes)
+    .use('/article', sanitizeBody, articleRoutes);
 
   return routes;
 };
