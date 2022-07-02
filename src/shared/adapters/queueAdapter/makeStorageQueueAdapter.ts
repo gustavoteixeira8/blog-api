10b2534b9762731b82ddfec9ecad9697659e8696
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-07-02 17:30:09 -0300

    Improve factory return

--- src/shared/adapters/queueAdapter/makeStorageQueueAdapter.ts
+++ src/shared/adapters/queueAdapter/makeStorageQueueAdapter.ts
@@ -1,6 +1,7 @@
 import { queueConfig } from '@config/queue';
+import { StorageOptions } from '../storageAdapter/StorageAdapterProtocol';
 import { BullQueueAdapter } from './implementations/BullQueueAdapter';
 
-export const makeStorageQueueAdapter = () => {
+export const makeStorageQueueAdapter = (): BullQueueAdapter<StorageOptions> => {
   return new BullQueueAdapter('storage-queue', queueConfig.storageQueue);
 };
