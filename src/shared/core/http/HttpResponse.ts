5b151f9173eca480ad55bd359df0bb2f113b7f84
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-07-11 21:42:15 -0300

    Add new http responses

--- src/shared/core/http/HttpResponse.ts
+++ src/shared/core/http/HttpResponse.ts
@@ -1,8 +1,6 @@
-import { HttpErrorProtocol } from '@shared/infra/http/errors/httpErrors';
-
 export interface Body {
-  message: string | string[] | null;
-  data: Record<string, any> | null;
+  message?: string | string[] | null;
+  data?: Record<string, any> | null;
 }
 
 export interface HttpResponse {
@@ -13,8 +11,8 @@ export interface HttpResponse {
 export const created = ({ data, message }: Body): HttpResponse => {
   return {
     body: {
-      data,
-      message,
+      data: data || null,
+      message: message || null,
     },
     status: 201,
   };
@@ -23,19 +21,63 @@ export const created = ({ data, message }: Body): HttpResponse => {
 export const ok = ({ data, message }: Body): HttpResponse => {
   return {
     body: {
-      data,
-      message,
+      data: data || null,
+      message: message || null,
     },
     status: 200,
   };
 };
 
-export const errorResponse = (error: HttpErrorProtocol): HttpResponse => {
+export const unauthorized = ({ data, message }: Body): HttpResponse => {
   return {
     body: {
-      message: error.errors,
-      data: null,
+      data: data || null,
+      message: message || null,
     },
-    status: error.status,
+    status: 401,
+  };
+};
+
+export const forbidden = ({ data, message }: Body): HttpResponse => {
+  return {
+    body: {
+      data: data || null,
+      message: message || null,
+    },
+    status: 403,
+  };
+};
+
+export const badRequest = ({ data, message }: Body): HttpResponse => {
+  return {
+    body: {
+      data: data || null,
+      message: message || null,
+    },
+    status: 400,
+  };
+};
+
+export const notFound = ({ data, message }: Body): HttpResponse => {
+  return {
+    body: {
+      data: data || null,
+      message: message || null,
+    },
+    status: 404,
+  };
+};
+
+export const defaultResponse = ({
+  data,
+  message,
+  status,
+}: Body & { status: number }): HttpResponse => {
+  return {
+    body: {
+      data: data || null,
+      message: message || null,
+    },
+    status: status,
   };
 };
