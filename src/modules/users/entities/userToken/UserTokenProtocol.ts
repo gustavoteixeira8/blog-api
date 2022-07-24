53ff0c149e6ebe001e1d3a9d8033d553680490ca
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-07-24 11:08:54 -0300

    Fix reference error

--- src/modules/users/entities/userToken/UserTokenProtocol.ts
+++ src/modules/users/entities/userToken/UserTokenProtocol.ts
@@ -1,12 +1,12 @@
 import { TimestampProtocol } from '@shared/core/entities/TimestampProtocol';
 import { ForeignKeyId } from '@shared/core/valueObjects/ForeignKeyId';
 import { Identifier } from '@shared/core/valueObjects/Identifier';
-import { TokenJWT } from '@shared/core/valueObjects/TokenJWT';
+import { TokenSpecific } from '@shared/core/valueObjects/TokenSpecific';
 import { TokenType } from '@shared/core/valueObjects/TokenType';
 
 export interface UserTokenProtocol extends Pick<TimestampProtocol, 'createdAt'> {
   readonly id?: Identifier;
-  token: TokenJWT;
+  token: TokenSpecific;
   userId: ForeignKeyId;
   expiresIn: Date;
   type: TokenType;
