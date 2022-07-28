2fe4fdb11fef60f871c322611b25e7e400207510
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-07-28 17:59:46 -0300

    Remove unused adapters

--- src/modules/users/useCases/sendVerificationEmail/makeSendVerificationEmail.ts
+++ src/modules/users/useCases/sendVerificationEmail/makeSendVerificationEmail.ts
@@ -3,7 +3,6 @@ import { SendVerificationEmailUseCase } from './SendVerificationEmailUseCase';
 import { WebController } from '@shared/core/controllers/WebController';
 import { makeDateAdapter } from '@shared/adapters/dateAdapter/makeDateAdapter';
 import { makeMailQueueAdapter } from '@shared/adapters/queueAdapter/makeMailQueueAdapter';
-import { makeTokenAdapter } from '@shared/adapters/tokenAdapter/makeTokenAdapter';
 import { makeUserRepository } from '../../repositories/implementations/makeUserRepository';
 import { makeUserTokenRepository } from '../../repositories/implementations/makeUserTokenRepository';
 
@@ -11,13 +10,11 @@ export const makeSendVerificationEmail = (): WebController => {
   const userRepository = makeUserRepository();
   const userTokenRepository = makeUserTokenRepository();
   const dateAdapter = makeDateAdapter();
-  const tokenAdapter = makeTokenAdapter();
   const mailQueueAdapter = makeMailQueueAdapter();
   const useCase = new SendVerificationEmailUseCase(
     userRepository,
     userTokenRepository,
     dateAdapter,
-    tokenAdapter,
     mailQueueAdapter,
   );
   return new SendVerificationEmailController(useCase);
