6061cb8afe619143727299d0eefa3fddfeb559bb
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-07-28 22:05:17 -0300

    Add cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,18 @@
 // import { UnauthorizedError } from '@shared/infra/http/errors/httpErrors';
+import { UnauthorizedError } from '@shared/core/http/httpErrors';
 import { CorsOptions } from 'cors';
 
 const allowedDomains = [
-  'https://gustavo.gq',
-  process.env.MY_LOCALHOST_IP,
-  process.env.MY_LOCALHOST,
+  'https://blog.gustavo.gq',
+  // process.env.MY_LOCALHOST_IP,
+  // process.env.MY_LOCALHOST,
 ];
 
 export const corsConfig = {
   origin: (origin, cb) => {
     if (!allowedDomains.includes(origin || 'WRONG_ORIGIN')) {
-      // cb(new UnauthorizedError('Not allowed by CORS'));
-      // return;
+      cb(new UnauthorizedError('Not allowed by CORS'));
+      return;
     }
 
     cb(null, origin);
