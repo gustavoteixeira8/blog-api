093c0337cb444624e5bb5e8f69f2a0790646aaaf
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-10-28 11:23:09 -0300

    improve token VO

--- src/shared/core/valueObjects/TokenSpecific.ts
+++ src/shared/core/valueObjects/TokenSpecific.ts
@@ -1,12 +1,15 @@
 import { ValueObjectProtocol } from './ValueObjectProtocol';
 import { InvalidTokenError } from '@shared/core/errors';
+import { makeUuidAdapter } from '@shared/adapters/uuidAdapter/makeUuidAdapter';
 
 export class TokenSpecific extends ValueObjectProtocol<string> {
+  private static _uuidAdapter = makeUuidAdapter();
+
   public static create(token?: string): TokenSpecific | InvalidTokenError {
     return new TokenSpecific(token || this.generate());
   }
 
   private static generate(): string {
-    return `${Date.now()}${Math.random().toString(36).slice(2)}`;
+    return `${Date.now()}-${this._uuidAdapter.generate()}`;
   }
 }
